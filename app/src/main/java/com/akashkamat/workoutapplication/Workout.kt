60a3e56d01feb598b420b0c8f0fe2c3adae09d89
AkashKamatCreates <kamatakash13@gmail.com>
2024-02-26 14:44:57 +0530

    Automatic Day Detection and workout listing accordingly

--- app/src/main/java/com/akashkamat/workoutapplication/Workout.kt
+++ app/src/main/java/com/akashkamat/workoutapplication/Workout.kt
@@ -1,3 +1,5 @@
 package com.akashkamat.workoutapplication
 
-data class Workout(val name: String, val totalSets: Int)
+data class Workout(val name: String, val totalSets: Int) {
+    val checkedStates = BooleanArray(totalSets)
+}
